5669b5e392ce018356ffaeaaa2f9f83108a468f4
Mustansar Hussain <mustansar3755hussain@gmail.com>
2025-08-10 11:00:01 +0500

    Project 21 - Dice Roll

--- 21 - Dice Roller/script.js
+++ 21 - Dice Roller/script.js
@@ -1,20 +1,19 @@
-// Function to update diece values
+function diceRoll() {
+  const dice1=Math.floor(Math.random()*6)+1;
+  const dice2=Math.floor(Math.random()*6)+1;
 
-function rollDice() {
-  // alert('Button Is Working')
-  const dice1 = Math.floor(Math.random() * 6) + 1;
-  const dice2 = Math.floor(Math.random() * 6) + 1;
+  document.getElementById('dice1').src=`image/dice${dice1}.png`
+  document.getElementById('dice2').src=`image/dice${dice2}.png`
 
-  document.getElementById("dice1").src = `image/dice${dice1}.png`
-  document.getElementById("dice2").src = `image/dice${dice2}.png`
-  let resultText = '';
-  if (dice1 > dice2) {
-    resultText = "Player 1 Wins!🚩"
-  } else if (dice2 > dice1) {
-    resultText = "Player 2 Wins!🚩"
-  } else {
-    resultText = "It's a Draw"
+  let resultText="";
+
+  if(dice1>dice2){
+    resultText="🚩 Player 1 Wins!"
+  }else if(dice2>dice1){
+    resultText="🚩 Player 2 Wins!"
+  }else{
+    resultText="It's a Draw Match"
   }
-  document.getElementById('result').innerText = resultText
+
+  document.getElementById('result').innerText=resultText;
 }
- 
